2c65ba15b3e2107aac74e7c5c77e6738a264c204
Rafazanella01 <rafazanella2004@gmail.com>
2024-01-15 09:12:42 -0300

    Alterações

--- index.php
+++ index.php
@@ -1,15 +1,17 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>PHP Hello World </title>
 </head>
 <body>
+
+<h1>PHP Hello World </h1>
     <?php
 
-        echo "hello world";
-
+        echo "hello world. <br>";
+        echo "oiii";
 
     ?>
 </body>
